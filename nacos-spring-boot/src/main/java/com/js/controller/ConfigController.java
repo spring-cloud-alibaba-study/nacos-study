c2c9e5f054abb3a59a8adddca1079ec2813088ad
姜爽 <jiangshuang@wejoydata.com>
2022-04-20 16:22:41 +0800

    fix -> 一般来说生产和测试不会是一套配置中心

--- nacos-spring-boot/src/main/java/com/js/controller/ConfigController.java
+++ nacos-spring-boot/src/main/java/com/js/controller/ConfigController.java
@@ -3,18 +3,19 @@ package com.js.controller;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.js.config.TestProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigController {
-//    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
-//    private boolean useLocalCache;
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
     @Autowired
     private TestProperties testProperties;
 
     @GetMapping(value = "/get")
     public String get() {
-        return testProperties.toString() + "：";
+        return testProperties.toString() + "：" + useLocalCache ;
     }
 }
