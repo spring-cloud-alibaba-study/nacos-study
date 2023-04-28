64f175fb26150626ffda9d534ac40cd0d7544c31
jiangshuang <jiangshuang@kanzhun.com>
2023-04-28 11:11:26 +0800

    git push

---
+++ nacos-spring-boot/src/main/java/com/js/config/Constants.java
@@ -0,0 +1,7 @@
+package com.js.config;
+
+public class Constants {
+
+    public static final String NACOS_DATAID = "${nacos.config.data-id}";
+    public static final String NACOS_GROUP = "${nacos.config.group}";
+}
--- nacos-spring-boot/src/main/java/com/js/config/TestProperties.java
+++ nacos-spring-boot/src/main/java/com/js/config/TestProperties.java
@@ -6,11 +6,14 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import static com.js.config.Constants.NACOS_DATAID;
+import static com.js.config.Constants.NACOS_GROUP;
+
 @Data
 @Component
 @ConfigurationProperties(prefix = TestProperties.TEST_PRO_KEY)
-@NacosConfigurationProperties(prefix = TestProperties.TEST_PRO_KEY, dataId = "nacos-spring-boot",
-        type = ConfigType.YAML, autoRefreshed = true,groupId = "operation")
+@NacosConfigurationProperties(prefix = TestProperties.TEST_PRO_KEY, dataId = NACOS_DATAID,
+        type = ConfigType.YAML, autoRefreshed = true,groupId = NACOS_GROUP)
 public class TestProperties {
 
     public static final String TEST_PRO_KEY = "my.test";
