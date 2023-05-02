9dd0eaddca37f8abc26b3d72deb100b8e9590e48
dujie <jjiangshuang@gmail.com>
2023-05-02 08:38:36 +0800

    update version

--- nacos-spring-boot/src/main/java/com/js/controller/ConfigController.java
+++ nacos-spring-boot/src/main/java/com/js/controller/ConfigController.java
@@ -1,12 +1,15 @@
 package com.js.controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.js.config.TestProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ConfigController {
     @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
     private boolean useLocalCache;
@@ -15,6 +18,6 @@ public class ConfigController {
 
     @GetMapping(value = "/get")
     public String get() {
-        return testProperties.toString() + "：" + useLocalCache ;
+        return JSON.toJSONString(testProperties.toString()) ;
     }
 }
